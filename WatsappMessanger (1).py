00ffefa7ef8e4eebc88a0de0edad1afeb2a6d199
Makailus <145263157+Makailus@users.noreply.github.com>
2023-09-21 17:48:13 +0530

    Add files via upload

---
+++ WatsappMessanger (1).py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pywhatkit
+
+
+# In[2]:
+
+
+import pywhatkit
+pywhatkit.sendwhatmsg("number","message",time(hrs),time(min))
+
+
+# In[ ]:
+
+
+
+
