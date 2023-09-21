7524bd9c9c6e38c3a4ee6dead43bc4fa957c4897
Makailus <145263157+Makailus@users.noreply.github.com>
2023-09-21 17:49:52 +0530

    Update WatsappMessanger (1).py

--- WatsappMessanger (1).py
+++ WatsappMessanger (1).py
@@ -1,21 +1,2 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import pywhatkit
-
-
-# In[2]:
-
-
-import pywhatkit
-pywhatkit.sendwhatmsg("number","message",time(hrs),time(min))
-
-
-# In[ ]:
-
-
-
-
+pywhatkit.sendwhatmsg("number","message",time(hrs),time(min)) #enter number and message and time(in 24hrs format) when you want to send the message  
